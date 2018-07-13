e46996f74d1f52a45720e7f669bbab02c96799cf
Tudor Maxim <tudor.maxim@takeofflabs.com>
2018-07-13 16:53:07 +0300

    start profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  attr_accessor :first_name, :last_name, :admin, :banned, :victories, :email
+  # attr_accessor :first_name, :last_name, :admin, :banned, :victories, :email
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
 
   def full_name
-    "#{@first_name}" + "#{@last_name}"
+    first_name + " " + last_name
   end
+
 end
