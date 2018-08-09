90e2ce1f173b4b4354e3db9cf2549855951a907c
Tudor Maxim <tudor.maxim@takeofflabs.com>
2018-08-09 21:56:16 +0300

    Notification reversed to have the newest one the first

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,6 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = Notification.where(recipient: current_user).unread.paginate(page: params[:page])
+    @notifications = Notification.where(recipient: current_user).unread.newest.paginate(page: params[:page])
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,6 +4,7 @@ class Notification < ApplicationRecord
   belongs_to :notifiable, polymorphic: true
 
   scope :unread, -> { where(read_at: nil) }
+  scope :newest, -> { order(:created_at).reverse_order }
 
   def read
     self.update_attributes(read_at: Time.now)
